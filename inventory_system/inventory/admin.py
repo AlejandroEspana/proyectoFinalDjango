92c7ee1f1cf6e2a591c5d8b97adfc315ef7dbce6
C-David-Burbano-C <david.burbano@simerelectronics.com>
2026-05-21 08:02:42 -0500

    Register inventory and transaction models in admin

--- inventory_system/inventory/admin.py
+++ inventory_system/inventory/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Supplier, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    search_fields = ('name', 'email')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'supplier', 'stock', 'price')
+    list_filter = ('category', 'supplier')
+    search_fields = ('name', 'category__name', 'supplier__name')
--- inventory_system/transactions/admin.py
+++ inventory_system/transactions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'transaction_type',
+        'quantity',
+        'unit_price',
+        'total_price',
+        'date',
+        'user',
+    )
+    list_filter = ('transaction_type', 'date', 'user')
+    search_fields = ('product__name', 'user__username')
